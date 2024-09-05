2025935d49c339ba3b789946a378d544efabd12e
Ankita Paul <146657863+Anki492@users.noreply.github.com>
2024-09-05 20:56:04 +0530

    Delete circlearea.java

--- circlearea.java
+++
@@ -1,7 +0,0 @@
-public class circlearea {
-    public static void main(String[] args){
-        int r=5;
-        double area=Math.PI*r*r;
-        System.out.println("area of circle "+area);
-    }
-}
